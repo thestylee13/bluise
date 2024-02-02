a77c517a69a490da3e6f82e3cdea5bf76ff68171
thestylee13 <74990921+thestylee13@users.noreply.github.com>
2024-02-02 11:59:44 +0200

    asd
    asdasdasd

---
+++ app/pages/test.vue
@@ -0,0 +1,8 @@
+{
+    "slug": "about",
+    "title": "About Us",
+    "content": "<p>Welcome to our website! Here's some information about our team and mission.</p><p>...</p>",
+    "seoDescription": "Learn more about our team and what we do.",
+    "seoMetaImage": "url_to_your_image.jpg"
+  }
+  
